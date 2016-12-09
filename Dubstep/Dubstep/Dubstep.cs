02361303eb292b5029dae9a515c3f7287a2e0c52
Patrick Mulder <pcmulder89@gmail.com>
2016-12-09 12:31:07 +0100

    Init

---
+++ Dubstep/Dubstep/Dubstep.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+public class Dubstep
+{
+    public static string SongDecoder(string input)
+    {
+        return string.Join(" ", input.Split(new string[] { "WUB" }, int.MaxValue, StringSplitOptions.RemoveEmptyEntries));
+    }
+}
---
+++ Dubstep/DubstepTest/UnitTest1.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+
+namespace DubstepTest
+{
+    [TestFixture]
+    public class DubstepTests
+    {
+        [Test]
+        public void Test1()
+        {
+            Assert.AreEqual("ABC", Dubstep.SongDecoder("WUBWUBABCWUB"));
+        }
+
+        [Test]
+        public void Test2()
+        {
+            Assert.AreEqual("R L", Dubstep.SongDecoder("RWUBWUBWUBLWUB"));
+        }
+    }
+}
