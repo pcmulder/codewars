e0da2ea1c0d09556fbf195032b6c391428ff2a9c
Patrick Mulder <pcmulder89@gmail.com>
2016-12-09 14:34:53 +0100

    Finished the Triple Trouble kata

---
+++ Triple Trouble/Triple Trouble Test/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+
+[TestFixture]
+public class Tests
+{
+    [Test]
+    [TestCase(451999277, 41177722899, ExpectedResult = 1)]
+    [TestCase(1222345, 12345, ExpectedResult = 0)]
+    [TestCase(12345, 12345, ExpectedResult = 0)]
+    [TestCase(666789, 12345667, ExpectedResult = 1)]
+    [TestCase(10560002, 100, ExpectedResult = 1)]
+    public static int FixedTest(long s1, long s2)
+    {
+        return Kata.TripleDouble(s1, s2);
+    }
+}
---
+++ Triple Trouble/Triple Trouble/Kata.cs
@@ -0,0 +1,35 @@
+﻿public class Kata
+{
+    public static int TripleDouble(long num1, long num2)
+    {
+        return ContainsStraight(num1, 3) && ContainsStraight(num2, 2) ? 1 : 0;
+    }
+
+    private static bool ContainsStraight(long num, int length)
+    {
+        char[] chars = num.ToString().ToCharArray();
+
+        int count = 1;
+        char compareTo = chars[0];
+
+        for (int i = 1; i < chars.Length; i++)
+        {
+            if (chars[i] == compareTo)
+            {
+                count++;
+
+                if (count == length)
+                {
+                    return true;
+                }
+            }
+            else
+            {
+                compareTo = chars[i];
+                count = 1;
+            }
+        }
+
+        return false;
+    }
+}
